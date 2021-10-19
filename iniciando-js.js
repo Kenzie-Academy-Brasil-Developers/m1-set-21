af1a4450a23bfaac3c2b719067210c96306178db
Isabella Bertini <isabella@kenzie.com.br>
2021-10-19 10:35:14 -0300

    Variaveis e seus tipos

---
+++ iniciando-js.js
@@ -0,0 +1,43 @@
+// + - * / %
+
+// 2*3 => 2+2+2
+
+//variaveis
+//{} []
+
+//LET CONST
+
+function returnCar(){
+    let car = "Land Rover";
+
+    if(car === "Land Rover"){
+       car = "Ferrari";
+       let car2 = "outro carro";
+
+        console.log("Dentro do if:" + car);
+    }
+    console.log("Fora do if:" + car);
+    console.log("Fora do if:" + car2);
+}
+
+for(let i = 2000; i <= 2010; i++){
+    console.log(i);
+}
+console.log(i);
+
+
+//CONST
+const IP = '127.0.0.1';
+//console.log(IP);
+
+const Fruta = "Banana";
+const fruta = "Banana";
+
+//STRING - TEXTO '' "" => "2154687"
+//NUMBER - 12248
+//BOOLEANO - TRUE FALSE
+
+let numero = boolean;
+
+
+//FUNÇÃO - CONJUNTO DE INSTRUÇÕES
