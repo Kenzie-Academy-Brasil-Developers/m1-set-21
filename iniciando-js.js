987c45e4180508f53226374683ff68ab13197db5
Isabella Bertini <isabella@kenzie.com.br>
2021-10-21 10:56:48 -0300

    Array

--- iniciando-js.js
+++ iniciando-js.js
@@ -14,19 +14,19 @@ function returnCar(){
        car = "Ferrari";
        let car2 = "outro carro";
 
-        console.log("Dentro do if:" + car);
+        //console.log("Dentro do if:" + car);
     }
-    console.log("Fora do if:" + car);
-    console.log("Fora do if:" + car2);
+    //console.log("Fora do if:" + car);
+    //console.log("Fora do if:" + car2);
 
-    console.log(car);
+    //console.log(car);
 
     return car;
 }
 
 //LAÇO DE REPETIÇÃO = LOOP
 for(let i = 2000; i <= 2010; i++){
-    console.log(i);
+   // console.log(i);
 }
 //console.log(i);
 
@@ -59,7 +59,7 @@ function soma(number1,number2){
 
     return resultado;
 }
-
+soma(2,3);
 
 
 
@@ -69,13 +69,13 @@ function sum(number1,number2){
 
     if(number1 === 2 && number2 === 3){
         soma = number1 + number2;
-        console.log("Entrou IF");
+        //console.log("Entrou IF");
     }else if(number2 === 3){
         soma = number1 + number2 + number2;
-        console.log("Entrou no segundo IF");
+        //console.log("Entrou no segundo IF");
     }else{
         soma = number1 + number1;
-        console.log("ENTROU NO ELSE");
+        //console.log("ENTROU NO ELSE");
     }
 
     return soma;    
@@ -104,7 +104,7 @@ for(let i = 0; i <= 10; i++){
     }else{
         console.log("Impar:" + i);
     }
-    console.log(i);
+   console.log(i);
 }
 
 
@@ -114,9 +114,58 @@ if(2 === 2){
     //retorno
 }
 
-let variavel = 2 === 2 ? console.log(true) : console.log(false);
+//let variavel = 2 === 2 ? console.log(true) : console.log(false);
 
 
 // = atribuo valor
 // == é igual a algo 
 // === é identico
+
+//ARRAY => armazenar um conjunto de valores dentro da variavel
+
+const casa1 = "Amarela";
+const casa2 = "Vermelha";
+const casa3 = "Azul";
+
+const casa = ["Amarela", "Vermelha", "Azul"];
+
+//adiciona na posição especifica
+casa[3] = "Verde";
+casa[1] = "Pink";
+
+console.log(casa);
+
+//adiciono um elemento dentro do array
+casa.push("Verde"); //adiciona no final do array
+casa.unshift("Verde"); //adiciona no começo do array
+casa.pop("Azul"); //retira o elemento do array
+
+console.log(casa);
+
+//saber a quantidade de elementos dentro do array
+const quantidade = casa.length;
+//console.log(quantidade);
+
+//----------------------------------------------------------
+const isabella = ["Isabella", 27, "São Calos", "SP", true];
+console.log(isabella);
+
+const numeros = [1,2,3,4,5,6,7,8,9,10];
+console.log(numeros);
+
+for(let i = 0; i <= 10; i++){
+    if(i % 2 === 0){
+        //console.log("Par:" + i);
+    }else{
+        //console.log("Impar:" + i);
+    }
+    //console.log(i);
+}
+
+for(let i = 0; i < numeros.length; i++){
+    if(numeros[i] % 2 === 0){
+        console.log("Par:" + numeros[i]);
+    }else{
+        console.log("Impar:" + numeros[i]);
+    }
+}
