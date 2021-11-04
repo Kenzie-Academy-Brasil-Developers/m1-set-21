efb8ce5649993588cb9aad5ef9931bd18fbd134a
Isabella Bertini <isabella@kenzie.com.br>
2021-11-04 10:54:50 -0300

    Metodos de array

--- metodos.js
+++ metodos.js
@@ -33,9 +33,9 @@ const metodoSlice = 'banana';
 //a = 5
 
 let numbrs = metodoSlice.length / 2;
-console.log(numbrs);
+//console.log(numbrs);
 
-console.log(metodoSlice.slice(numbrs));
+//console.log(metodoSlice.slice(numbrs));
 
 //REPLACE
 
@@ -52,17 +52,17 @@ let metodoReplace = 'Texto para o replace';
 
 const arrNumber = [3,4,8,1,7];
 
-console.log(arrNumber.indexOf(3));
+//console.log(arrNumber.indexOf(3));
 
 
 if(arrNumber.indexOf(3) !== -1){
-    console.log("ok");
+    //console.log("ok");
 }else{
-    console.log("não ok");
+    ////console.log("não ok");
 }
 
 let existe = arrNumber.indexOf(3) !== -1 ? true : false;
-console.log(existe);
+//console.log(existe);
 
 //for(let i = 0; i < arrNumber.length; i++){
     //let existe = arrNumber[i] === 3 ? console.log("ok") : console.log("não ok");
@@ -71,3 +71,64 @@ console.log(existe);
     //console.log(existe);
     //console.log(position);
 //}
+
+//-------------------METODOS DE ARRAY-----------------------------------//
+
+//INCLUDES - utiliza ele pra pesquisar um paramentro dentro do array, com retorno de true ou false
+
+const str = 'Texto para o includes encontrar a palavra';
+const numberArr = [1,2,3,6];
+
+console.log(str.includes('includes'));
+console.log(str.includes('abobrinha'));
+
+console.log(numberArr.includes(3));
+console.log(numberArr.includes(8));
+
+//FILTER - filtra dentro do array com o paramentro passado e retornar um novo array com os elementos
+//que compoem a condição
+
+const wordsArr = ['mouse', 'teclado', 'monitor', 'tomada', 'energia'];
+
+const result = wordsArr.filter(word => word.length > 6);
+
+console.log(result);
+//function verificaPalavra(word){
+    //return word.length > 6;
+//}
+
+let cities = [
+    { name:'Rio de Janeiro', temperatura: 40 },
+    { name:'Porto Alegre', temperatura: 27 },
+    { name:'São Paulo', temperatura: 32 },
+    { name: 'Curitiba', temperatura: 25 }
+];
+
+//if
+//for
+let temperaturaBaixa = [];
+for(let i = 0; i < cities.length; i++){
+    if(cities[i].temperatura < 30){
+        temperaturaBaixa.push(cities[i]);
+    }
+}
+console.log(temperaturaBaixa);
+
+temperaturaBaixa = cities.filter(city => city.temperatura < 30);
+
+console.log(temperaturaBaixa);
+
+//INDEXOF
+const frutas = ['banana', 'manga', 'morango', 'manga'];
+console.log(frutas.indexOf('manga'));
+
+//FIND
+const numerosArr = [1,2,3,5,8,9,11,13,15];
+const number = numerosArr.find(elemento => elemento > 10);
+
+console.log(number);
+
+//CONCAT
+const doisArr = numerosArr.concat(frutas);
+console.log(doisArr);
+
